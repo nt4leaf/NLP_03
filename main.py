03a31c486e2a304a41c9516aec93e4e1da0efae5
Tuan Nguyen <22010508@st.phenikaa-uni.edu.vn>
2024-11-06 11:16:40 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,8 @@
 import streamlit as st
 from sub1 import model_pred
 
-video_id = st.text_input('Enter YouTube URL')
-st.markdown(
-    """
-    <meta http-equiv="refresh" content="5">
-    """,
-    unsafe_allow_html=True
-)
+video_id = st.text_input('Enter Video ID')
+
 if video_id:
     model_pred(video_id)
 
